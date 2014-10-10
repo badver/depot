58a145cbc1311a29e7dd2e0947ad18f4d6d9e710
badver <badver@mail.ru>
2014-10-10 12:12:57 +0400

    tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :description, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }, presence: true
+  validates :title, uniqueness: true, presence: true
+  validates :image_url, presence: true, format: {
+      with: %r{\.(gif|jpg|png)\Z}i,
+      message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
