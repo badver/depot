a9425fff71731888aad176a8e4f1dbb6f92100fe
badver <badver@mail.ru>
2014-10-13 16:46:56 +0400

    add i18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'admin' => 'admin#index'
+
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
@@ -11,16 +12,18 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
 
   get 'store/index'
   resources :products do
     get :who_bought, on: :member
   end
 
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
